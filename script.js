8f08faed21c6a53d1cb925db140b3b1be67f5d4a
Charmaine Matoro <charmainmatoro@gmail.com>
2026-06-24 13:31:31 +0200

    Add click event listener to navigation links

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const navLinks = document.querySelectorAll(".nav-links a");
 
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    console.log("Section opened:", link.textContent);
+  });
+});
